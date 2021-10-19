cb119af7e65d88a8cb760b8267948a1acbf4a9af
franklinnnn <86860477+franklinnnn@users.noreply.github.com>
2021-10-19 10:21:17 -0700

    Update blackjack.js

--- portfolio/projects/blackjack.js
+++ portfolio/projects/blackjack.js
@@ -1,58 +1,188 @@
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
-let messageEl = document.getElementById("message-el")
-let pSum = document.getElementById("p-sum")
-let pCards = document.getElementById("p-cards")
+let suits = ["<span class='red'>&#9829 </span>", 
+             "<span class='red'>&#9830 </span>",
+             "<span class='black'>&#9827 </span>",
+             "<span class='black'>&#9824 </span>"];
 
-function getCard(){
-    let number = Math.floor(Math.random() * 13) + 1
-    if (number > 10){
-        return 10
-    } 
-    else if (number === 1){
-        return 11
-    }
-    else{
-        return number
-    }
-}
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let message = document.getElementById("message");
+let pcards = document.getElementById("player-cards");
+let pscore = document.getElementById("player-score");
+let dcards = document.getElementById("dealer-cards");
+let dscore = document.getElementById("dealer-score");
+let text = document.getElementById("text-area");
+let start = document.getElementById("start-btn");
+let hit = document.getElementById("hit-btn");
+let stay = document.getElementById("stay-btn");
+
+let gameStart = false;
+let gameOver = false;
+let gameWin = false;
+let playerCards = [];
+let dealerCards = [];
+let playerScore = 0;
+let dealerScore = 0;
+let deck = [];
 
 function startGame(){
-    isAlive = true
-    let firstCard = getCard()
-    let secondCard = getCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    makeGame()
+    gameStarted = true;
+    gameOver = false;
+    gameWin = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    playerCards = [getCard(), getCard()];
+    dealerCards = [getCard(), getCard()];
+    showStatus();
+
+    text.textContent = "";
 }
 
-function makeGame(){
-    pCards.innerHTML = "Cards: "
-    for(let i = 0; i < cards.length; i++){
-        pCards.innerHTML += cards[i]+" "
+function createDeck(){
+    let deck = [];
+    for(let i = 0; i < suits.length; i++){
+        for(let j = 0; j < values.length; j++){
+            let card = {suit: suits[i], value: values[j]}
+            deck.push(card);
+        }
     }
-
-    pSum.innerHTML = "Sum: "+ sum
-    if( sum <= 20){
-        message = "Draw new card?"
+    return deck;
+}
+function shuffleDeck(deck){
+    for(let i = 0; i < deck.length; i++){
+        let shuffle = Math.trunc(Math.random() * deck.length);
+        let temp = deck[shuffle];
+        deck[shuffle] = deck[i];
+        deck[i] = temp;
     }
-    else if(sum === 21){
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    }
-    else{
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.innerHTML = message
 }
 
-function newCard(){
-    let card = getCard()
-    sum += card
-    cards.push(card)
-    makeGame()
+function hitBtn(){
+    playerCards.push(getCard());
+    checkEnd();
+    showStatus();
+}
+function stayBtn(){
+    gameOver = true;
+    checkEnd();
+    showStatus();
+}
+
+function checkEnd(){
+    updateScores();
+
+    if(gameOver){
+        while(dealerScore < playerScore && playerScore <= 21 && dealerScore <= 21){
+            dealerCards.push(getCard());
+            updateScores();
+        }
+    }
+
+    if(playerScore > 21){
+        gameWin = false;
+        gameOver = true;
+    }
+    else if(dealerScore > 21){
+        gameWin = true;
+        gameOver = true;
+    }
+    else if(gameOver){
+        if(playerScore > dealerScore){
+            gameWin = true;
+        }
+        else{
+            gameWin = false;
+        }
+    }
+}
+
+function getCardString(card){
+        return "<div class='card'>" + card.value + card.suit + "</div>";
+}
+function getCardValue(card){
+        switch(card.value){
+        case "A":
+            return 1;
+        case "2":
+            return 2;
+        case "3":
+            return 3;
+        case "4":
+            return 4;
+        case "5":
+            return 5;
+        case "6":
+            return 6;
+        case "7":
+            return 7;
+        case "8":
+            return 8;
+        case "9":
+            return 9;
+        default:
+            return 10;       
+    }
+}
+
+
+function showStatus(){
+    if(!gameStarted){
+        //message.innerHTML = "Play a game";
+        text.innerHTML = "Play a game";
+        return;
+    }
+    let playerCardString = '';
+    for(let i = 0; i < playerCards.length; i++){
+        playerCardString += getCardString(playerCards[i]);
+    }
+    let dealerCardString = '';
+    for(let i = 0; i < dealerCards.length; i++){
+        dealerCardString += getCardString(dealerCards[i]);
+    }
+    updateScores();
+
+    // message.innerHTML = dealerCardString + dealerScore + "\n\n" + playerCardString + playerScore;
+    
+    dcards.innerHTML = dealerCardString;
+    dscore.innerHTML = dealerScore;
+    pcards.innerHTML = playerCardString;
+    pscore.innerHTML = playerScore;
+
+    if(gameOver){
+        if(gameWin){
+            //message.innerHTML += "You win";
+            text.innerHTML += "You win!";
+      }
+        else{
+            //message.innerHTML += "Dealer wins";
+            text.innerHTML += "Dealer wins";
+        }
+    }
+}
+
+function getScore(cardArray){
+    let score = 0;
+    let ace = false;
+    for(let i = 0; i < cardArray.length; i++){
+        let card = cardArray[i];
+        score += getCardValue(card);
+        if(card.value == "A"){
+            ace = true;
+        }
+        if(ace && score + 10 <= 21){
+            return score + 10;
+        }
+    }
+    return score;
+}
+
+function updateScores(){
+    playerScore = getScore(playerCards);
+    dealerScore = getScore(dealerCards);
+}
+
+function getCard(){ 
+    let c = deck.shift();
+    return c;
+    //return deck.shift();
 }
