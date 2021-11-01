5d9fb9d8114c43fead1a4bd50fd8084800d6aa4e
franklinnnn <86860477+franklinnnn@users.noreply.github.com>
2021-11-01 14:29:55 -0700

    Update blackjack.js

--- portfolio/projects/blackjack.js
+++ portfolio/projects/blackjack.js
@@ -17,24 +17,31 @@ let stay = document.getElementById("stay-btn");
 
 let gameStart = false;
 let gameOver = false;
-let gameWin = false;
+let gameStatus = 0;
 let playerCards = [];
 let dealerCards = [];
 let playerScore = 0;
 let dealerScore = 0;
 let deck = [];
 
+start.style.display = 'inline';
+hit.style.display = 'none';
+stay.style.display = 'none';
+
 function startGame(){
     gameStarted = true;
+    gameStatus = 0;
     gameOver = false;
-    gameWin = false;
 
     deck = createDeck();
     shuffleDeck(deck);
     playerCards = [getCard(), getCard()];
     dealerCards = [getCard(), getCard()];
+    start.innerHTML = "Restart";
+    hit.style.display = 'inline';
+    stay.style.display = 'inline';
     showStatus();
-
+    checkEnd();
     text.textContent = "";
 }
 
@@ -70,28 +77,33 @@ function stayBtn(){
 
 function checkEnd(){
     updateScores();
-
+    
     if(gameOver){
         while(dealerScore < playerScore && playerScore <= 21 && dealerScore <= 21){
             dealerCards.push(getCard());
             updateScores();
         }
     }
-
     if(playerScore > 21){
-        gameWin = false;
+        gameStatus = 2;
         gameOver = true;
     }
     else if(dealerScore > 21){
-        gameWin = true;
+        gameStatus = 1;
         gameOver = true;
     }
     else if(gameOver){
         if(playerScore > dealerScore){
-            gameWin = true;
+            gameStatus = 1;
         }
-        else{
-            gameWin = false;
+        else if(playerScore = 21){
+            gameStatus = 1;
+        }
+        else if(dealerScore > playerScore){
+            gameStatus = 2;
+        }
+        else if(playerScore == dealerScore){
+            gameStatus = 0;
         }
     }
 }
@@ -145,27 +157,39 @@ function showStatus(){
     pcards.innerHTML = playerCardString;
     pscore.innerHTML = playerScore;
 
+
     if(gameOver){
-        if(gameWin){
-            text.innerHTML += "You win!";
-      }
-        else{
-            text.innerHTML += "Dealer wins";
+        if(gameStatus == 1){
+            win();
         }
+        else if(gameStatus == 2){
+            text.innerHTML += "Dealer wins"
+            //lose();
+        }
+        else if(gameStatus == 0){
+            //text.innerHTML = "It's a tie";
+            tie();
+        }
+        start.style.display = 'inline';
+        hit.style.display = 'none';
+        stay.style.display = 'none';
     }
 }
 
 function getScore(cardArray){
     let score = 0;
-    let ace = false;
+    ace = 0;
     for(let i = 0; i < cardArray.length; i++){
         let card = cardArray[i];
         score += getCardValue(card);
+
         if(card.value == "A"){
-            ace = true;
+            ace += 1;
         }
-        if(ace && score + 10 <= 21){
-            return score + 10;
+    }
+    for(let j = 0; j < ace; j++){
+        if(score + 10 <= 21){
+            score += 10;
         }
     }
     return score;
@@ -174,6 +198,36 @@ function getScore(cardArray){
 function updateScores(){
     playerScore = getScore(playerCards);
     dealerScore = getScore(dealerCards);
+}
+
+function win(){
+    updateScores();
+    if(playerScore === 21){
+        text.innerHTML = "You have Blackjack!";
+    }
+    else if(dealerScore > 21){
+        text.innerHTML = "Dealer bust! You win";
+    }
+}
+
+function lose(){
+    updateScores();
+    if(playerScore > 21){
+        text.innerHTML += "Bust! Dealer wins";
+    }
+    else if(playerScore < dealerScore){
+        text.innerHTML += "Dealer wins"
+    }
+    else{
+        text.innerHTML += "Dealer wins"
+    }
+}
+
+function tie(){
+    updateScores();
+    if(playerScore = dealerScore){
+        text.innerHTML = "It's a tie"
+    }
 }
 
 function getCard(){ 
